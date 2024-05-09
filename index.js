3a7b2833b7bebdc8ad1072abe3ff59de6acbdf9d
Ryan <coap.ryanmontgomery@gmail.com>
2024-05-09 05:04:12 -0600

    Creates myFile.md

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Your code here
+function moveDodgerLeft() {
+    let dodger = document.getElementById('dodger');
+    let leftNumbers = dodger.style.left.replace('px', '');
+    let left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+  
+  function moveDodgerRight() {
+    let dodger = document.getElementById('dodger');
+    let leftNumbers = dodger.style.left.replace('px', '');
+    let left = parseInt(leftNumbers, 10);
+  
+    // Assuming the game's width is 400px
+    if (left < 360) {
+      dodger.style.left = `${left + 1}px`;
+    }
+  }
+  
